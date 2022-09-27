2cc055f78be0891926cb442dbf9c3616f1da4afd
Conrado Pereira <conrado.spereira@gmail.com>
2022-09-27 00:52:12 -0300

    Implementing method getById.

--- src/main/java/br/com/mercadoteixeira/inventorycontrol/controller/ProductsController.java
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/controller/ProductsController.java
@@ -1,10 +1,14 @@
 package br.com.mercadoteixeira.inventorycontrol.controller;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,12 @@ public class ProductsController {
     @GetMapping
     public Page<ProductsDto> list(@PageableDefault(size = 10) Pageable pagination) {
         return service.getAll(pagination);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductsDto> detail(@PathVariable @NotNull Long id) {
+        ProductsDto dto = service.getById(id);
+
+        return ResponseEntity.ok(dto);
     }
 }
