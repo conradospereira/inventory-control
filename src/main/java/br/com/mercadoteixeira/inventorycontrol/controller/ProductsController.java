8d735008e8382b67c3dca421d52ec646335f38ff
Conrado Pereira <conrado.spereira@gmail.com>
2022-09-27 21:53:45 -0300

    Creating post, put and delete method on controller

--- src/main/java/br/com/mercadoteixeira/inventorycontrol/controller/ProductsController.java
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/controller/ProductsController.java
@@ -1,5 +1,8 @@
 package br.com.mercadoteixeira.inventorycontrol.controller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +10,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.mercadoteixeira.inventorycontrol.dto.ProductsDto;
 import br.com.mercadoteixeira.inventorycontrol.service.ProductsService;
@@ -32,5 +40,25 @@ public class ProductsController {
         ProductsDto dto = service.getById(id);
 
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductsDto> register(@RequestBody @Valid ProductsDto dto, UriComponentsBuilder uriBuilder) {
+        ProductsDto products = service.registerProduct(dto);
+        URI addres = uriBuilder.path("/products/{id}").buildAndExpand(products.getId()).toUri();
+
+        return ResponseEntity.created(addres).body(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductsDto> update(@PathVariable @NotNull Long id, @RequestBody @Valid ProductsDto dto) {
+        ProductsDto updated = service.updatedProducts(id, dto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductsDto> remove(@PathVariable @NotNull Long id) {
+        service.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
