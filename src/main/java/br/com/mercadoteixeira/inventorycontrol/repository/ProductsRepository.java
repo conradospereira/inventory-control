7019c27e1d0ff9ea519eb0b1d769bf31d5a945e6
Conrado Pereira <conrado.spereira@gmail.com>
2022-10-09 22:44:08 -0300

    Fixing syntaxe.

--- src/main/java/br/com/mercadoteixeira/inventorycontrol/repository/ProductsRepository.java
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/repository/ProductsRepository.java
@@ -1,9 +1,10 @@
 package br.com.mercadoteixeira.inventorycontrol.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.mercadoteixeira.inventorycontrol.model.Products;
-
+@Repository
 public interface ProductsRepository extends JpaRepository<Products, Long>{
     
 }
