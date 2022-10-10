56f3cf9613e4818d2a1b026a249f551f1581394b
Conrado Pereira <conrado.spereira@gmail.com>
2022-10-09 22:44:34 -0300

    Refactoring.

--- src/main/resources/db/migration/V1__create_table_products.sql
+++ src/main/resources/db/migration/V1__create_table_products.sql
@@ -1,7 +1,8 @@
 CREATE TABLE products (
     id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    price DECIMAL(19,2) NOT NULL,
     description VARCHAR(100) NOT NULL,
+    code DECIMAL(13) NOT NULL,
+    price DECIMAL(19,2) NOT NULL,
     amount VARCHAR(255),
     PRIMARY KEY (id)
 );
