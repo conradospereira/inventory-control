982d6b0486cc4c18ab4670cff8bf03d823df1c94
Conrado Pereira <conrado.spereira@gmail.com>
2022-10-25 23:14:00 -0300

    Creating class repository to find user by username.

---
+++ src/main/java/br/com/mercadoteixeira/inventorycontrol/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.mercadoteixeira.inventorycontrol.repository;
+
+import br.com.mercadoteixeira.inventorycontrol.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+}
